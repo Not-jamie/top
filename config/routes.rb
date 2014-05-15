5ded3bae6cb2f59533f5981de160b67d9800e5d6
Ross Gordon <r.gordon.06@aberdeen.ac.uk>
2014-05-15 17:37:19 +0100

    links etc added

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ root :to => 'musics#index'
   post "music/search" => 'music#show'
   post "music/filter" => 'music#show'
   post "music/show" => 'music#show'
+    get "/index" => 'music#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :users
